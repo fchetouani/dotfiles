e7c1702a1199fdbc6638236471edaaa1ae664436
Farid Chetouani <farid.chetouani@vidal.fr>
2024-02-14 15:48:02 +0100

    Add aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,10 @@
+alias cls='clear' # Good 'ol Clear Screen command
+
+alias dockerbo="DOCKER_HOST=tcp://dockerbo.vidal.net:4243 DOCKER_API_VERSION=1.22 docker"
+alias dockersw="DOCKER_HOST=tcp://dockersw.vidal.net:4243 DOCKER_API_VERSION=1.22 docker"
+
+alias h='history'
+
 alias reload!='. ~/.zshrc'
 
-alias cls='clear' # Good 'ol Clear Screen command
+alias rm='rm -i'
